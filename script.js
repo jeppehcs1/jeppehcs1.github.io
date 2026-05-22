1fda3fb03f8efb2541421ba00cf7e2f1c529abcd
jeppehcs1 <s235223@dtu.dk>
2026-05-22 10:46:02 +0200

    the

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function dingDongButton() {
   const dingdongs = ["jake2.PNG", "rallemus.PNG", "rallemus2.PNG", "dingdong1.PNG", "dingdong2.jpg",
                       "dingdong3.jpg","dingdong4.jpg","dingdong5.PNG","dingdong6.jpg","dingdong7.jpg",
                       "dingdong8.PNG","dingdong9.PNG","dingdong10.PNG","dingdong11.PNG","dingdong12.PNG",
-                      "dingdong13.PNG","dingdong14.PNG","dingdong15.PNG"
+                      "dingdong13.PNG","dingdong14.PNG","dingdong15.PNG", "The ding dong.jpg"
   ];
 
   if (image.style.display === 'none' || image.style.display === '') {
